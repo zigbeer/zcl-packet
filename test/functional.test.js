b23f9437839552227f570b15644e189c6d8fb5e2
Jack Wu <jackchased@gmail.com>
2016-06-21 15:40:37 +0800

    testing are done.

---
+++ test/functional.test.js
@@ -0,0 +1,151 @@
+var expect = require('chai').expect,
+    Chance = require('chance'),
+    chance = new Chance(),
+    zclId = require('zcl-id');
+
+var zclmeta = require('../lib/zclmeta'),
+    FuncClass = require('../lib/functional');
+
+var clusterIds = [],
+    k;
+
+for (k in zclId._common.clusterId) {
+    clusterIds.push(k);
+}
+
+describe('Functional Cmd framer and parser Check', function() {
+    clusterIds.forEach(function (cluster) {
+        var cInfo = zclId._getCluster(cluster),
+            cmdIds = [];
+
+        if (!cInfo || !cInfo.cmd) return;
+
+        cInfo.cmd.enums.forEach(function (cmdObj) {
+            cmdIds.push(cmdObj.key);
+        });
+
+        cmdIds.forEach(function (cmd) {
+            var funcObj,
+                reqParams,
+                payload,
+                args = {};
+
+            reqParams = zclmeta.functional.getParams(cluster, cmd);
+
+            reqParams.forEach(function (arg) {
+                args[arg.name] = randomArg(arg.type);
+            });
+
+            funcObj = new FuncClass(cluster, 0, cmd);
+            payload = funcObj.frame(args);
+
+            funcObj.parse(payload, function (err, result) {
+                it(funcObj.cmd + ' frame() and parse() check', function () {
+                        expect(result).to.eql(args);
+                });
+            });
+        });
+    });
+});
+
+describe('Functional CmdRsp framer and parser Check', function() {
+    clusterIds.forEach(function (cluster) {
+        var cInfo = zclId._getCluster(cluster),
+            cmdRspIds = [];
+
+        if (!cInfo || !cInfo.cmdRsp) return;
+
+        cInfo.cmdRsp.enums.forEach(function (cmdObj) {
+            cmdRspIds.push(cmdObj.key);
+        });
+
+        cmdRspIds.forEach(function (cmdRsp) {
+            var funcObj,
+                reqParams,
+                payload,
+                args = {};
+
+            reqParams = zclmeta.functional.getParams(cluster, cmdRsp);
+
+            reqParams.forEach(function (arg) {
+                args[arg.name] = randomArg(arg.type);
+            });
+
+            funcObj = new FuncClass(cluster, 1, cmdRsp);
+            payload = funcObj.frame(args);
+
+            funcObj.parse(payload, function (err, result) {
+                it(funcObj.cmd + ' frame() and parse() check', function () {
+                    expect(result).to.eql(args);
+                });
+            });
+        });
+    });
+});
+
+function randomArg(type) {
+    var testBuf,
+        testArr,
+        k;
+
+    switch (type) {
+        case 'uint8':
+            return chance.integer({min: 0, max: 255});
+        case 'uint16':
+            return chance.integer({min: 0, max: 65535});
+        case 'uint32':
+            return chance.integer({min: 0, max: 4294967295});
+        case 'int8' :
+            return chance.integer({min: -128, max: 127});
+        case 'int16' :
+            return chance.integer({min: -32768, max: 32767});
+        case 'int32' :
+            return chance.integer({min: -2147483648, max: 2147483647});
+        case 'floatle':
+            return chance.floating({min: 0, max: 4294967295});
+        case 'longaddr':
+            return '0x00124b00019c2ee9';
+        case 'stringPreLen':
+            var stringLen = chance.integer({min: 0, max: 255});
+            return chance.string({length: stringLen});
+        case 'preLenUint8':
+        case 'preLenUint16':
+        case 'preLenUint32':
+            return 10;
+        case 'dynUint8':
+        case 'dynUint16':
+        case 'dynUint24':
+        case 'dynUint32':
+            testArr = [];
+            for (k = 0; k < 10; k += 1) {
+                if (type === 'dynUint8')
+                    testArr[k] = chance.integer({min: 0, max: 255});
+                else if (type === 'dynUint16')
+                    testArr[k] = chance.integer({min: 0, max: 65535});
+                else if (type === 'dynUint24')
+                    testArr[k] = chance.integer({min: 0, max: 16777215});
+                else if (type === 'dynUint32')
+                    testArr[k] = chance.integer({min: 0, max: 4294967295});
+            }
+            return testArr;
+        case 'locationbuffer':
+            testBuf = new Buffer(16);
+            for (k = 0; k < 16; k += 1) {
+                testBuf[k] = chance.integer({min: 0, max: 255});
+            }
+            return testBuf;
+        case 'zonebuffer': 
+            testArr = [];
+            for (k = 0; k < 20; k += 2) {
+                testArr[k] = chance.integer({min: 0, max: 255});
+                testArr[k + 1] = chance.integer({min: 0, max: 65535});
+            }
+            return testArr;
+        case 'extfieldsets':
+            return [ { clstId: 0x0006, len: 0x3, extField: [0x01, 0x02, 0x03]}, { clstId: 0x0009, len: 0x5, extField: [0x05, 0x04, 0x03, 0x02, 0x01]} ];
+        default:
+            break;
+    }
+
+    return;
+}
--- test/zcl.test.js
+++ test/zcl.test.js
@@ -3,20 +3,125 @@ var expect = require('chai').expect,
 
 describe('APIs Arguments Check for Throwing Error', function() {
     describe('#.frame', function() {
+        var frameCntl = {frameType:1, manufSpec: 0, direction: 0, disDefaultRsp: 1};
+
         it('should be a function', function () {
             expect(zcl.frame).to.be.a('function');
+        });
+
+        it('should throw TypeError if input frameCntl is not an object', function () {
+            expect(function () { return zcl.frame(undefined, 0, 0, 'toggle', {}, 'genOnOff'); }).to.throw(TypeError);
+            expect(function () { return zcl.frame(null, 0, 0, 'toggle', {}, 'genOnOff'); }).to.throw(TypeError);
+            expect(function () { return zcl.frame(NaN, 0, 0, 'toggle', {}, 'genOnOff'); }).to.throw(TypeError);
+            expect(function () { return zcl.frame([], 0, 0, 'toggle', {}, 'genOnOff'); }).to.throw(TypeError);
+            expect(function () { return zcl.frame(true, 0, 0, 'toggle', {}, 'genOnOff'); }).to.throw(TypeError);
+            expect(function () { return zcl.frame(new Date(), 0, 0, 'toggle', {}, 'genOnOff'); }).to.throw(TypeError);
+            expect(function () { return zcl.frame(function () {}, 0, 0, 'toggle', {}, 'genOnOff'); }).to.throw(TypeError);
+
+            expect(function () { return zcl.frame(frameCntl, 0, 0, 'toggle', {}, 'genOnOff'); }).not.to.throw(TypeError);
+        });
+
+        it('should throw TypeError if input manufCode is not a number', function () {
+            expect(function () { return zcl.frame(frameCntl, undefined, 0, 'toggle', {}, 'genOnOff'); }).to.throw(TypeError);
+            expect(function () { return zcl.frame(frameCntl, null, 0, 'toggle', {}, 'genOnOff'); }).to.throw(TypeError);
+            expect(function () { return zcl.frame(frameCntl, NaN, 0, 'toggle', {}, 'genOnOff'); }).to.throw(TypeError);
+            expect(function () { return zcl.frame(frameCntl, [], 0, 'toggle', {}, 'genOnOff'); }).to.throw(TypeError);
+            expect(function () { return zcl.frame(frameCntl, true, 0, 'toggle', {}, 'genOnOff'); }).to.throw(TypeError);
+            expect(function () { return zcl.frame(frameCntl, new Date(), 0, 'toggle', {}, 'genOnOff'); }).to.throw(TypeError);
+            expect(function () { return zcl.frame(frameCntl, function () {}, 0, 'toggle', {}, 'genOnOff'); }).to.throw(TypeError);
+        });
+
+        it('should throw TypeError if input seqNum is not a number', function () {
+            expect(function () { return zcl.frame(frameCntl, 0, undefined, 'toggle', {}, 'genOnOff'); }).to.throw(TypeError);
+            expect(function () { return zcl.frame(frameCntl, 0, null, 'toggle', {}, 'genOnOff'); }).to.throw(TypeError);
+            expect(function () { return zcl.frame(frameCntl, 0, NaN, 'toggle', {}, 'genOnOff'); }).to.throw(TypeError);
+            expect(function () { return zcl.frame(frameCntl, 0, [], 'toggle', {}, 'genOnOff'); }).to.throw(TypeError);
+            expect(function () { return zcl.frame(frameCntl, 0, true, 'toggle', {}, 'genOnOff'); }).to.throw(TypeError);
+            expect(function () { return zcl.frame(frameCntl, 0, new Date(), 'toggle', {}, 'genOnOff'); }).to.throw(TypeError);
+            expect(function () { return zcl.frame(frameCntl, 0, function () {}, 'toggle', {}, 'genOnOff'); }).to.throw(TypeError);
+        });
+
+        it('should throw TypeError if input cmd is not a number and not a string', function () {
+            expect(function () { return zcl.frame(frameCntl, 0, 0, undefined, {}, 'genOnOff'); }).to.throw(TypeError);
+            expect(function () { return zcl.frame(frameCntl, 0, 0, null, {}, 'genOnOff'); }).to.throw(TypeError);
+            expect(function () { return zcl.frame(frameCntl, 0, 0, NaN, {}, 'genOnOff'); }).to.throw(TypeError);
+            expect(function () { return zcl.frame(frameCntl, 0, 0, [], {}, 'genOnOff'); }).to.throw(TypeError);
+            expect(function () { return zcl.frame(frameCntl, 0, 0, true, {}, 'genOnOff'); }).to.throw(TypeError);
+            expect(function () { return zcl.frame(frameCntl, 0, 0, new Date(), {}, 'genOnOff'); }).to.throw(TypeError);
+            expect(function () { return zcl.frame(frameCntl, 0, 0, function () {}, {}, 'genOnOff'); }).to.throw(TypeError);
+
+            expect(function () { return zcl.frame(frameCntl, 0, 0, 'toggle', {}, 'genOnOff'); }).not.to.throw(TypeError);
+            expect(function () { return zcl.frame(frameCntl, 0, 0, 2, {}, 'genOnOff'); }).not.to.throw(TypeError);
+        });
+
+        it('should throw TypeError if input zclPayload is not an object and not an array', function () {
+            expect(function () { return zcl.frame(frameCntl, 0, 0, 'toggle', undefined, 'genOnOff'); }).to.throw(TypeError);
+            expect(function () { return zcl.frame(frameCntl, 0, 0, 'toggle', null, 'genOnOff'); }).to.throw(TypeError);
+            expect(function () { return zcl.frame(frameCntl, 0, 0, 'toggle', NaN, 'genOnOff'); }).to.throw(TypeError);
+            expect(function () { return zcl.frame(frameCntl, 0, 0, 'toggle', true, 'genOnOff'); }).to.throw(TypeError);
+
+            expect(function () { return zcl.frame(frameCntl, 0, 0, 'toggle', {}, 'genOnOff'); }).not.to.throw(TypeError);
+            expect(function () { return zcl.frame(frameCntl, 0, 0, 'toggle', [], 'genOnOff'); }).not.to.throw(TypeError);
+        });
+
+        it('should throw TypeError if input clusterId is not a number and not a string', function () {
+            expect(function () { return zcl.frame(frameCntl, 0, 0, 'toggle', {}, undefined); }).to.throw(TypeError);
+            expect(function () { return zcl.frame(frameCntl, 0, 0, 'toggle', {}, null); }).to.throw(TypeError);
+            expect(function () { return zcl.frame(frameCntl, 0, 0, 'toggle', {}, NaN); }).to.throw(TypeError);
+            expect(function () { return zcl.frame(frameCntl, 0, 0, 'toggle', {}, []); }).to.throw(TypeError);
+            expect(function () { return zcl.frame(frameCntl, 0, 0, 'toggle', {}, true); }).to.throw(TypeError);
+            expect(function () { return zcl.frame(frameCntl, 0, 0, 'toggle', {}, new Date()); }).to.throw(TypeError);
+            expect(function () { return zcl.frame(frameCntl, 0, 0, 'toggle', {}, function () {}); }).to.throw(TypeError);
+
+            expect(function () { return zcl.frame(frameCntl, 0, 0, 'toggle', {}, 'genOnOff'); }).not.to.throw(TypeError);
+            expect(function () { return zcl.frame(frameCntl, 0, 0, 'toggle', {}, 6); }).not.to.throw(TypeError);
         });
     });
 
     describe('#.parse', function() {
+        var zclBuf = new Buffer([0x11, 0x00, 0x02]);
+
         it('should be a function', function () {
             expect(zcl.parse).to.be.a('function');
+        });
+
+        it('should throw TypeError if input zclBuf is not a buffer', function () {
+            expect(function () { return zcl.parse(undefined, 0, function () {}); }).to.throw(TypeError);
+            expect(function () { return zcl.parse(null, 0, function () {}); }).to.throw(TypeError);
+            expect(function () { return zcl.parse(NaN, 0, function () {}); }).to.throw(TypeError);
+            expect(function () { return zcl.parse([], 0, function () {}); }).to.throw(TypeError);
+            expect(function () { return zcl.parse(true, 0, function () {}); }).to.throw(TypeError);
+            expect(function () { return zcl.parse(new Date(), 0, function () {}); }).to.throw(TypeError);
+            expect(function () { return zcl.parse(function () {}, 0, function () {}); }).to.throw(TypeError);
+        });
+
+        it('should throw TypeError if input clusterId is not a number and not a string', function () {
+            expect(function () { return zcl.parse(zclBuf, undefined, function () {}); }).to.throw(TypeError);
+            expect(function () { return zcl.parse(zclBuf, null, function () {}); }).to.throw(TypeError);
+            expect(function () { return zcl.parse(zclBuf, NaN, function () {}); }).to.throw(TypeError);
+            expect(function () { return zcl.parse(zclBuf, [], function () {}); }).to.throw(TypeError);
+            expect(function () { return zcl.parse(zclBuf, true, function () {}); }).to.throw(TypeError);
+            expect(function () { return zcl.parse(zclBuf, new Date(), function () {}); }).to.throw(TypeError);
+            expect(function () { return zcl.parse(zclBuf, function () {}, function () {}); }).to.throw(TypeError);
+
+            expect(function () { return zcl.parse(zclBuf, 'genOnOff', function () {}); }).not.to.throw(TypeError);
+            expect(function () { return zcl.parse(zclBuf, 6, function () {}); }).not.to.throw(TypeError);
         });
     });
 
     describe('#.header', function() {
         it('should be a function', function () {
             expect(zcl.header).to.be.a('function');
+        });
+
+        it('should throw TypeError if input buf is not a buffer', function () {
+            expect(function () { return zcl.header(undefined); }).to.throw(TypeError);
+            expect(function () { return zcl.header(null); }).to.throw(TypeError);
+            expect(function () { return zcl.header(NaN); }).to.throw(TypeError);
+            expect(function () { return zcl.header([]); }).to.throw(TypeError);
+            expect(function () { return zcl.header(true); }).to.throw(TypeError);
+            expect(function () { return zcl.header(new Date()); }).to.throw(TypeError);
+            expect(function () { return zcl.header(function () {}); }).to.throw(TypeError);
         });
     });
 });
